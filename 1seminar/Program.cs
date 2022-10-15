59e5c1420cb47c0c9980ca6dee5dcc9c280fe6db
belka.boo <ibelka.boo@gmail.com>
2022-10-15 12:22:37 +0300

    compile 2

--- 1seminar/Program.cs
+++ 1seminar/Program.cs
@@ -1,4 +1,20 @@
 ﻿// Домашнее задание, задача 2
 Console.Clear();
+Console.WriteLine("Введите 2 числа: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+int NumberB = Convert.ToInt32(Console.ReadLine());
 
+if (NumberA < NumberB)
+{
+    Console.WriteLine(NumberB);
+    
+}
+if (NumberA == NumberB)
+{
+    Console.WriteLine("Числа равны");
 
+}
+else
+{
+    Console.WriteLine(NumberA);
+}
