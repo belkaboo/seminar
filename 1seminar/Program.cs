90f43451899c1058f16220e4e40dc778b19011e0
belka.boo <ibelka.boo@gmail.com>
2022-10-15 23:57:50 +0300

    work

--- 1seminar/Program.cs
+++ 1seminar/Program.cs
@@ -4,17 +4,17 @@ Console.WriteLine("Введите 2 числа: ");
 int NumberA = Convert.ToInt32(Console.ReadLine());
 int NumberB = Convert.ToInt32(Console.ReadLine());
 
-if (NumberA < NumberB)
-{
-    Console.WriteLine(NumberB);
-    
-}
 if (NumberA == NumberB)
 {
     Console.WriteLine("Числа равны");
 
 }
-else
+if (NumberA < NumberB)
 {
-    Console.WriteLine(NumberA);
+    Console.WriteLine($"Большее число = {NumberB} ");
+    
+}
+if (NumberA > NumberB)
+{
+    Console.WriteLine($"Большее число = {NumberA} ");
 }
