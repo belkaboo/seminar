d8400d1b352aa397b058d753be9f5f83af9d6704
belka.boo <ibelka.boo@gmail.com>
2022-10-16 01:09:41 +0300

    work2

--- 2seminar/Program.cs
+++ 2seminar/Program.cs
@@ -1,5 +1,19 @@
 ﻿// Задача 4
-console.Clear();
+Console.Clear();
 
+Console.WriteLine("Введите числа: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+int NumberB = Convert.ToInt32(Console.ReadLine());
+int NumberC = Convert.ToInt32(Console.ReadLine());
 
+int max = NumberA;
+if (NumberB > max)
+    {
+        max = NumberB;
+    }
+if (NumberC > max)
+    {
+        max = NumberC;
+    }
+Console.WriteLine ($"Большее число = {max}");
 
