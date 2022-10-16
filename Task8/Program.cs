1d3da334a0ecdb6d1c0d095c3403884ca010c7ae
belka.boo <ibelka.boo@gmail.com>
2022-10-16 17:33:32 +0300

    compile+

---
+++ Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿// задача 8
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// проверка числа
+if (N < 2)
+{
+ Console.WriteLine("Введите число побольше: ");  
+ N = Convert.ToInt32(Console.ReadLine()); 
+}
+// начало
+int index = 1;
+while (index < N)
+{
+    if (index % 2 == 0)
+        {
+        Console.Write($"{index}, ");
+        index++;
+        }
+    else
+    {
+    index++;
+    }
+}
+Console.Write(index);
